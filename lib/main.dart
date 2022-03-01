c97060af964ed5106b8c9b84924e582fcd2f2a68
GalihPrev <galihpreviand27@gmail.com>
2022-03-01 23:45:18 +0700

    Konversi_Suhu_Flutter

--- lib/main.dart
+++ lib/main.dart
@@ -37,22 +37,38 @@ class _MyAppState extends State<MyApp> {
             children: [
               inputSuhu(text1: text1),
               Hasil(kelvin: kelvin, reamor: reamor),
-              Container(
-                height: 50,
-                width: double.infinity,
-                child: ElevatedButton(
-                  onPressed: () {
-                    konversi();
-                    text1.clear();
-                  },
-                  child: Text(
-                    "Konversi",
-                    style: TextStyle(fontSize: 30),
-                  ),
-                ),
-              )
+              konvertButton(text1: text1, konversi: konversi)
             ],
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class konvertButton extends StatelessWidget {
+  konvertButton({
+    Key? key,
+    required this.text1,
+    required this.konversi,
+  }) : super(key: key);
+
+  final TextEditingController text1;
+  final Function konversi;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 50,
+      width: double.infinity,
+      child: ElevatedButton(
+        onPressed: () {
+          konversi();
+          text1.clear();
+        },
+        child: Text(
+          "Konversi",
+          style: TextStyle(fontSize: 30),
         ),
       ),
     );
